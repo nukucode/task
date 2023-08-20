e3455d553e2fe6ab7d3569d89fb648156a9e8a05
therogersak <therogersak@gmail.com>
2023-08-20 12:25:51 +0530

    🍱 update assets

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["lh3.googleusercontent.com", "cdn4.iconfinder.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "cdn4.iconfinder.com",
+      "images.unsplash.com",
+    ],
   },
 };
 
